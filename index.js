64629188f5df9db516458ada1a2e07bd1f7f8511
James Womack <jwomack@netflix.com>
2017-08-15 12:23:03 -0700

    feat: support destructuring require

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 
 const FS     = require('fs')
 const globby = require('globby')
-const r1     = /^(let|var|const) +([a-zA-Z_$][a-zA-Z0-9_$]*) +\= +(require)\((('|")[a-zA-Z0-9-_.\/]+('|"))\)/gm
-const r2     = /^(let|var|const) +([a-zA-Z_$][a-zA-Z0-9_$]*) +\= +(require)\((('|")[a-zA-Z0-9-_.\/]+('|"))\)\.([a-zA-Z][a-zA-Z0-9]+)/gm
+const r1     = /^(let|var|const) +([a-zA-Z_$][a-zA-Z0-9_$]*) +\= +(require)\((('|")[a-zA-Z0-9-_.\/]+('|"))\)/gm // const createStore = require('redux')
+const r2     = /^(let|var|const) +([a-zA-Z_$][a-zA-Z0-9_$]*) +\= +(require)\((('|")[a-zA-Z0-9-_.\/]+('|"))\)\.([a-zA-Z][a-zA-Z0-9]+)/gm // const createStore = require('redux').createStore
+const r3     = /^(let|var|const) +(\{\s*([a-zA-Z_$][a-zA-Z0-9_$]*)\s*\}) +\= +(require)\((('|")[a-zA-Z0-9-_.\/]+('|"))\)/gm // const { createStore } = require('redux')
 
 const args = process.argv.slice(2)
 
@@ -22,6 +23,7 @@ paths.forEach(function (p) {
 
 function replaceInFile(fp) {
   const result = FS.writeFileSync(fp, FS.readFileSync(fp, 'utf-8')
+    .replace(r3, `import { $3 } from $5`)
     .replace(r2, `import { $7 as $2 } from $4`)
     .replace(r1, `import $2 from $4`), 'utf-8')
   console.log(`> ${fp}`)
---
+++ test-input.js
@@ -0,0 +1,9 @@
+const createStore0 = require('redux')
+const createStore1 = require('redux').createStore
+const { createStore2 } = require('redux')
+
+console.dir({
+  createStore0,
+  createStore1,
+  createStore2
+})
