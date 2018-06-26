7f0c9db5e51b33777a192700d46835108c880ac2
陈帅 <qixian.cs@outlook.com>
2018-06-26 16:18:04 +0800

    add test

--- test-input.js
+++ test-input.js
@@ -1,7 +1,8 @@
 const createStore0 = require('redux')
 const createStore1 = require('redux').createStore
 const { createStore2 } = require('redux')
-
+const { Modal, DatePicker, Radio, Select, InputNumber, Table, message } = require('antd')
+require('index.less')
 console.dir({
   createStore0,
   createStore1,
