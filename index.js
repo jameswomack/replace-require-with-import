d013f3ce6c95c47b7f52ab3654b59ae5a70435f9
Mr. Test <mrtest@example.com>
2024-01-23 13:54:30 -0800

    Commit message

--- index.js
+++ index.js
@@ -8,26 +8,29 @@ const r3     = /^(let|var|const) +(\{\s*([a-zA-Z_$][a-zA-Z0-9_$]*)\s*\}) +\= +(r
 
 const args = process.argv.slice(2)
 
+
 if (!args.length) {
-  console.error('Please pass a directory glob to "replace-require-with-import"\n')
-  process.exit(1)
+    console.error('Please pass a directory glob to "replace-require-with-import"\n')
+    process.exit(1)
 }
+
+
 
 const paths = globby.sync(args)
 
 paths.forEach(function (p) {
-  if (!FS.statSync(p).isDirectory()) {
-    return replaceInFile(p)
-  }
+    if (!FS.statSync(p).isDirectory()) {
+        return replaceInFile(p)
+    }
 })
 
 function replaceInFile(fp) {
-  const result = FS.writeFileSync(fp, FS.readFileSync(fp, 'utf-8')
-    .replace(r3, `import { $3 } from $5`)
-    .replace(r2, `import { $7 as $2 } from $4`)
-    .replace(r1, `import $2 from $4`), 'utf-8')
-  console.log(`> ${fp}`)
-  return result
+    const result = FS.writeFileSync(fp, FS.readFileSync(fp, 'utf-8')
+        .replace(r3, 'import { $3 } from $5')
+        .replace(r2, 'import { $7 as $2 } from $4')
+        .replace(r1, 'import $2 from $4'), 'utf-8')
+    console.log(`> ${fp}`)
+    return result
 }
 
 console.info('Done!\n')
