e244ead1391ed98aa5624d8fa8b221f0c508b7ec
James Womack <james.womack@servicenow.com>
2024-01-24 23:47:31 -0800

    Oh yeahhhhh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const FS     = require('fs')
+const FS     = require('fs');
 const globby = require('globby')
 const r1     = /^(let|var|const) +([a-zA-Z_$][a-zA-Z0-9_$]*) +\= +(require)\((('|")[a-zA-Z0-9-_.\/]+('|"))\)/gm // const createStore = require('redux')
 const r2     = /^(let|var|const) +([a-zA-Z_$][a-zA-Z0-9_$]*) +\= +(require)\((('|")[a-zA-Z0-9-_.\/]+('|"))\)\.([a-zA-Z][a-zA-Z0-9]+)/gm // const createStore = require('redux').createStore
@@ -8,15 +8,13 @@ const r3     = /^(let|var|const) +(\{\s*([a-zA-Z_$][a-zA-Z0-9_$]*)\s*\}) +\= +(r
 
 const args = process.argv.slice(2)
 
-
-console.log(FS);
-
 if (!args.length) {
     console.error('Please pass a directory glob to "replace-require-with-import"\n')
     process.exit(1)
 }
 
 const paths = globby.sync(args)
+
 
 paths.forEach(function (p) {
     if (!FS.statSync(p).isDirectory()) {
